19762880a192647b18f32212285c94b4642562b6
ArpitKharwade <143011369+ArpitKharwade@users.noreply.github.com>
2025-01-03 12:22:53 +0530

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,34 @@
+const express =   require("express");
+const router = express.Router();
 
+const{login,signup} = require("../controllers/Auth");
+const{auth, isStudent, isAdmin} = require("../middlewares/auth");
+
+// router.post("./login",login);
+router.post("/signup",signup);
+router.post("/login",login);
+
+//testing protected routes for single middleware
+router.get("/test", auth, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for test',
+    });
+});
+
+//Protected routes
+router.get("/student", auth, isStudent, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for students',
+    });
+});
+
+router.get("/admin", auth, isAdmin, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for admin',
+    });
+})
+
+module.exports = router;
