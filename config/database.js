93e073d49d189cb690a1bc605d48dc8a28ad4635
ArpitKharwade <143011369+ArpitKharwade@users.noreply.github.com>
2025-01-03 12:10:06 +0530

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,1 +1,15 @@
-a
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then( () => {console.log("DB Connected Successfully")})
+    .catch( (err) => {
+        console.log("DB Connection Issues");
+        console.error(err);
+        process.exit(1);
+    })
+}
